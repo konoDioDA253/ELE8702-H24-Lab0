9d5b23d0b68c1bab1ea0cbc952c6ef337b766295
Bouh <bouh.abdillahi@polymtl.ca>
2024-01-22 19:21:41 -0500

    WIP : write function to output coords.txt file

--- lab0_eqn.py
+++ lab0_eqn.py
@@ -1,10 +1,22 @@
 ## Numéro d'équipe : 
 ## Bouh Abdillahi (Matricule : 1940646)
 ## Vincent Yves Nodjom (Matricule : 1944011)
+## STATE OF WIP : 
+# Question 1: est-ce que le fichier de sortie lab0_eqn_coords.txt doit séparer la dernière ligne d'antenne et la première ligne de UE avec une ligne vide?
+# Question 2: Quel est l'espace entre les colonnes du fichier lab0_eqn_coords.txt
+# Question 3: les IDs d'appareil dans lab0_eqn_coords.txt doivent avoir le moins de chiffre possible? 
+
 import sys
 import math
 import yaml
 import random
+
+# Variables GLOBAL
+numero_equipe = 'n'
+numero_lab = '0'
+
+# Germe de toutes les fonctions aléatoires
+random.seed(123)
 
 class Antenna:
     
@@ -14,6 +26,9 @@ class Antenna:
         self.scenario = None
         self.height = None
         self.frequency = None
+        # (PROF) Est-ce que c'est correct de modifier comme cela la classe?
+        self.coordx = None
+        self.coordy = None
 
     
 class UE:
@@ -24,6 +39,9 @@ class UE:
         self.app=app_name
         self.height = None
         self.los = True
+        # (PROF) Est-ce que c'est correct de modifier comme cela la classe?
+        self.coordx = None
+        self.coordy = None
 
 def fill_up_the_lattice(N, lh, lv, nh, nv):
     """Function appelée par get_rectangle_lattice_coords()"""
@@ -127,19 +145,66 @@ def read_yaml_file(fname):
     with open(fname,'r') as file:
         return yaml.safe_load(file)
 
-def gen_random_coords(param1, param2, ... ,param n):
+def gen_random_coords(fichier_de_cas):
     # Cette fonction doit générer les coordonées pour le cas de positionnement aléatoire
-    # TODO
-
-def foo1(param1, param2, ... , param n):
-
-    # ....
+    # TODO PRESENTABLE
+    longueur_geometry = fichier_de_cas['ETUDE_IMPORTANT']['GEOMETRY']['Surface']['rectangle']['length']
+    hauteur_geometry = fichier_de_cas['ETUDE_IMPORTANT']['GEOMETRY']['Surface']['rectangle']['height']
     
 
-def foo2 (param1, param2, ... ,param n):
-    # ....
+    x_aleatoire = random.randint(1, longueur_geometry)
+    y_aleatoire = random.randint(1, hauteur_geometry)
+    coordonnees_aleatoires = {x_aleatoire, y_aleatoire}
+    return coordonnees_aleatoires
 
-def lab0 (data_case):
+
+
+# fonction initialisant une liste de ues et assignant des coordonnées aléatoirement à chaque ue dans la liste
+def assigner_coordonnees_ues(fichier_de_cas):
+    liste_ues_avec_coordonnees = []
+
+    nombre_ues_ue1 = fichier_de_cas['ETUDE_IMPORTANT']['DEVICES']['UE1-App1']['number']
+    nombre_ues_ue2 = fichier_de_cas['ETUDE_IMPORTANT']['DEVICES']['UE2-App2']['number']
+    type_de_generation = fichier_de_cas['ETUDE_IMPORTANT']['UE_COORD_GEN']
+
+    for i in range(nombre_ues_ue1):
+        ue = UE(idx=len(liste_ues_avec_coordonnees), app_name='UE1-App1')
+        # (PROF) qu'est-ce qu'on fait si c'est pas aléatoire?
+        if (type_de_generation == 'a') :
+            coords = gen_random_coords(fichier_de_cas)
+        ue.coordx, ue.coordy = coords
+        liste_ues_avec_coordonnees.append(ue)
+
+    for i in range(nombre_ues_ue2):
+        ue = UE(idx=len(liste_ues_avec_coordonnees), app_name='UE2-App2')
+        # (PROF) qu'est-ce qu'on fait si c'est pas aléatoire?
+        if (type_de_generation == 'a') :
+            coords = gen_random_coords(fichier_de_cas)
+        ue.coordx, ue.coordy = coords
+        liste_ues_avec_coordonnees.append(ue)
+
+    return liste_ues_avec_coordonnees
+
+# fonction initialisant une liste de antennes et assignant des coordonnées selon la grille à chaque antenne
+def assigner_coordonnees_antennes(fichier_de_cas):
+    liste_antennes_avec_coordonnees = []
+
+    nombre_antennes = fichier_de_cas['ETUDE_IMPORTANT']['DEVICES']['Antenna1']['number']
+    type_de_generation = fichier_de_cas['ETUDE_IMPORTANT']['ANT_COORD_GEN']
+    
+    # (PROF) terrain_shape est-il Hard-Wired?
+    terrain_shape =  fichier_de_cas['ETUDE_IMPORTANT']['GEOMETRY']['Surface']
+    coords = gen_lattice_coords(terrain_shape, nombre_antennes)
+
+    for idx, coord in enumerate(coords):
+        antenna = Antenna(idx)
+        antenna.coordx, antenna.coordy = coord
+        liste_antennes_avec_coordonnees.append(antenna)
+
+    return liste_antennes_avec_coordonnees
+
+# Retourne une liste d'antennes et d'ues avec leurs coordonnées initialisées
+def lab0 ():
     #TODO ....
     # antennas est une liste qui contient les objets de type Antenna
     # ues est une liste qui contient les objets de type UE
@@ -151,20 +216,30 @@ def lab0 (data_case):
     # avant de faire le retour, les objets appartenant aux listes antennas et ues 
     # doivent avoir leur coordonées initialisées
     # CETTE FONCTION EST OBLIGATOIRE
+
+    #(PROF) nom du yaml est Hard-Wired?
+    fichier_de_cas = read_yaml_file("lab"+ numero_lab + "_eq" + numero_equipe + "_cas.yaml")
+    ues = assigner_coordonnees_ues(fichier_de_cas)
+    antennas = assigner_coordonnees_antennes(fichier_de_cas)
     return (antennas,ues)
 
-def treat_args() :
-    # cette fonction doit retourner le nom du fichier de cas à partir de l'interface de commande (CLI)
-    #... 
-    # TODO
-    #....
-    # CETTE FONCTION EST OBLIGATOIRE
-    # À noter que dans cette fonction il faut ajouter les vérifications qui s'imposent
-    # par exemple, nombre d'arguments appropriés, existance du fichier de cas, etc.
-    return case_file_name
+# def treat_args() :
+#     # cette fonction doit retourner le nom du fichier de cas à partir de l'interface de commande (CLI)
+#     #... 
+#     # TODO
+#     #....
+#     # CETTE FONCTION EST OBLIGATOIRE
+#     # À noter que dans cette fonction il faut ajouter les vérifications qui s'imposent
+#     # par exemple, nombre d'arguments appropriés, existance du fichier de cas, etc.
+#     return case_file_name
 
 def main():
-    case_file_name = treat_args()
+    # case_file_name = treat_args()
+
+    antennas, ues = lab0()
+
+    # TODO : appeler la fonction ecrire_fichier_de_coordonnees(antennes,ues)
+    print("TEST")
     #
     #TODO les instructions de main qui vont faire appel aux autres fonctions du programme
     #.....
